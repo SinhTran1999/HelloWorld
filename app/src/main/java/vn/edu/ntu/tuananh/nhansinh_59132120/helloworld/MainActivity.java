4c65161c9fc665efdf04bb35bb35f534e8e21469
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-04-10 16:15:27 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/tuananh/nhansinh_59132120/helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.ntu.tuananh.nhansinh_59132120.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
